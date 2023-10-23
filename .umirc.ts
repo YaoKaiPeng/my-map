1de3aaaf6cfc7eebb3946efb0f1c2590104fbeb8
姚凯鹏 <kaipeng.yao@qq.com>
2023-10-23 17:19:07 +0800

    f

--- .umirc.ts
+++ .umirc.ts
@@ -3,10 +3,6 @@ import { defineConfig } from 'umi'
 export default defineConfig({
   base: '/my-map',
   publicPath: '/my-map/',
-  routes: [
-    { path: '/', component: 'index' },
-    { path: '/docs', component: 'docs' },
-  ],
   headScripts: [
     'https://api.map.baidu.com/api?v=3.0&ak=5tvPbrYNfwc2gK0FFaFGsFsRqi9FIM3F',
   ],
