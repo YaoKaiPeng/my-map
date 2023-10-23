4c91b3341bd3dd842a244a8b74c4e5fed269a659
姚凯鹏 <kaipeng.yao@qq.com>
2023-10-23 17:05:43 +0800

    f

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'umi'
 
 export default defineConfig({
+  base: '/my-map',
   routes: [
     { path: '/', component: 'index' },
     { path: '/docs', component: 'docs' },
